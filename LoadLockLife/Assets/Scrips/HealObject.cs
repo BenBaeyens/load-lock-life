8fb6d775cf951933e2f0fa5ae4417b3efdb8a200
Kjip <kjipgamer@gmail.com>
2019-05-31 18:30:53 +0200

    Fixed godmode powerup not working with heals

--- LoadLockLife/Assets/Scrips/HealObject.cs
+++ LoadLockLife/Assets/Scrips/HealObject.cs
@@ -28,7 +28,7 @@ public class HealObject : MonoBehaviour {
     private void OnTriggerEnter(Collider other) {
         if(other.gameObject.name == "Player")
         {
-            if (other.gameObject.GetComponent<PlayerController>().gameObject.transform.localScale.x > other.gameObject.GetComponent<PlayerController>().maxSize)
+            if (other.gameObject.GetComponent<PlayerController>().gameObject.transform.localScale.x >= other.gameObject.GetComponent<PlayerController>().maxSize)
             {
                 other.gameObject.GetComponent<PlayerController>().Heal();
             } else
